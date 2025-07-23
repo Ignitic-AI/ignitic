232f3e9813be483cd7771258ca09289a4e1a3cd3
sami <msami.bese22seecs@student.nust.edu.pk>
2025-07-23 05:36:17 +0500

    correct start script

--- apps/ai-engine/main.py
+++ apps/ai-engine/main.py
@@ -25,7 +25,3 @@ app.include_router(n8n_credential_router, prefix='/api/v1')
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Workflow Manager Service"}
-
-if __name__ == "__main__":
-    PORT = int(os.getenv("PORT", 8001))
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
