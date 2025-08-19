d6a8956b989edd981642dee34de85fa9b6aa9227
sami <msami.bese22seecs@student.nust.edu.pk>
2025-08-19 20:14:01 +0500

    openrouter support

--- apps/ai-engine/services/agents/llms.py
+++ apps/ai-engine/services/agents/llms.py
@@ -1,13 +1,19 @@
-from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
 
 load_dotenv()
 
 
-llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+
+
+llm = ChatOpenAI(
+    model="deepseek/deepseek-chat-v3-0324:free",
+    api_key=OPENROUTER_API_KEY,
+    base_url="https://openrouter.ai/api/v1",
     temperature=0.1,
     max_tokens=4096,
 )
-
 
