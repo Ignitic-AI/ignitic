8c9a408271d0251019935370954dfc0d75f00c56
sami <msami.bese22seecs@student.nust.edu.pk>
2025-12-05 12:53:37 +0500

    twitter trends - get top twitter trends based on country

--- apps/mcp/servers/marketer_mcp.py
+++ apps/mcp/servers/marketer_mcp.py
@@ -1,4 +1,7 @@
 from mcp.server.fastmcp import FastMCP
+from servers.tools.social_media_marketing.twitter_trends import twitter_trends
 
 app = FastMCP("Marketer MCP", streamable_http_path="/")
+
+app.add_tool(twitter_trends)
 
