e6f675aeb9c969c4a97643c50f703cd92a071d78
sami <msami.bese22seecs@student.nust.edu.pk>
2025-12-08 19:00:09 +0500

    seo tool

--- apps/mcp/servers/marketer_mcp.py
+++ apps/mcp/servers/marketer_mcp.py
@@ -2,10 +2,12 @@ from mcp.server.fastmcp import FastMCP
 from servers.tools.social_media_marketing.twitter_trends import twitter_trends
 from servers.tools.social_media_marketing.tiktok_trends import tiktok_trends
 from servers.tools.advertising.facebook_ads_scraper import facebook_ads_scraper
+from servers.tools.Seo.site_domain_authority_seo import site_domain_authority_seo
 
 app = FastMCP("Marketer MCP", streamable_http_path="/")
 
 app.add_tool(twitter_trends)
 app.add_tool(tiktok_trends)
 app.add_tool(facebook_ads_scraper)
+app.add_tool(site_domain_authority_seo)
 
