58c2cee60effe88dcfa07eec1ca415e14a15d4e8
sami <msami.bese22seecs@student.nust.edu.pk>
2026-02-14 05:04:14 +0500

    new

--- apps/mcp/servers/product_researcher_mcp.py
+++ apps/mcp/servers/product_researcher_mcp.py
@@ -3,6 +3,7 @@ from servers.tools.product_researcher.product_google_dork import google_dork_sea
 from servers.tools.product_researcher.apify_amazon_search import apify_amazon_search
 from servers.tools.product_researcher.apify_ebay_scraper import apify_ebay_search
 from servers.tools.product_researcher.google_trends import google_trends
+from servers.tools.product_researcher.shopify_product_scraper import shopify_product_scraper
 
 app = FastMCP("Product Researcher MCP", streamable_http_path="/")
 
@@ -10,3 +11,4 @@ app.add_tool(google_dork_search)
 app.add_tool(apify_amazon_search)
 app.add_tool(apify_ebay_search)
 app.add_tool(google_trends)
+app.add_tool(shopify_product_scraper)
