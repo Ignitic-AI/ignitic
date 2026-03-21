56ab896a88b12c4879b7f638dcccde01fff785c7
Intellema <Nust.beeba@gmail.com>
2026-03-21 05:25:05 +0500

    FIX: added turbopack for optimization

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: false,
-
-  /* config options here */
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        ignored: [
-          // Add problematic files
-          "**/pagefile.sys",
-          "**/hiberfil.sys",
-        ],
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
